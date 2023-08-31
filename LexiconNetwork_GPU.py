54f70b0b51981a7477edc194aa00e1a3bd5ed223
tkck110213 <penetrate.wonder@gmail.com>
2023-09-01 03:42:22 +0900

    feat: phonological network

--- LexiconNetwork_GPU.py
+++ LexiconNetwork_GPU.py
@@ -8,6 +8,11 @@ import json
 import sys
 import pickle
 import os
+import Levenshtein
+import itertools
+import pyopenjtalk
+
+
 
 class LexiconNetwork:
     def __init__(self, vectorPath):
@@ -16,19 +21,20 @@ class LexiconNetwork:
         self.model = KeyedVectors.load_word2vec_format(vectorPath)
         print("[\033[2m+\033[m]\033[1;32m Sucsess to load word vectors !! \033[m")
 
-        self.wordlist = self.model.index_to_key
+        self.wordlist = [word.replace("#", "") for word in self.model.index_to_key]
         self.dimention = len(self.wordlist)
         self.wordVectors = cp.array([self.model.get_vector(word) for word in self.model.index_to_key])
-        self.LexiconNetwork = PropertyGraph()
+        self.SemanticNetwork = PropertyGraph()
+        self.PhonologicalNetwork = PropertyGraph()
 
-        self.makeLexiconNetwork()
+        self.makePhonologicalNetwork()
         
-    def makeLexiconNetwork(self):
+    def makeSemanticNetwork(self):
         """Add vertex(word) in lexicon netowrk graph"""
         print("[\033[2m+\033[m] Add word in lexicon network...")
-        vertDf = cudf.DataFrame({"id":[i for i in range(len(self.wordlist))], "label":self.wordlist, "reservior":cp.zeros(len(self.wordlist)), 
-                                 "inflow":cp.zeros(len(self.wordlist)), "outflow":cp.zeros(len(self.wordlist))})
-        self.LexiconNetwork.add_vertex_data(vertDf, vertex_col_name="id")
+        vertDf = cudf.DataFrame({"id":[i for i in range(self.dimention)], "label":self.wordlist, "reservior":cp.zeros(self.dimention), 
+                                 "inflow":cp.zeros(self.dimention), "outflow":cp.zeros(self.dimention)})
+        self.SemanticNetwork.add_vertex_data(vertDf, vertex_col_name="id")
         
         """Calc cosine similarity of each words"""
         print("[\033[2m+\033[m] Calc similarity of each words...")
@@ -41,7 +47,7 @@ class LexiconNetwork:
         
 
         """Add edge to lexicon netowrk graph"""
-        print("[\033[2m+\033[m] Add edge in lexicon network...")
+        print("[\033[2m+\033[m] Add edge in semantic network...")
         # make combination edges pair
         vertexList = cp.array(range(self.dimention))
         edgeList = cp.array(cp.meshgrid(vertexList, vertexList)).T.reshape(-1, 2)
@@ -52,16 +58,37 @@ class LexiconNetwork:
         edgeDf["weight"] = similarities
         # delete one of duplication edge pair
         edgeDf = edgeDf.dropna(how="any")
-        self.LexiconNetwork.add_edge_data(edgeDf, vertex_col_names=("src", "dst"))
-        print("[\033[2m+\033[m]\033[1;32m Sucsess to generate lexicon network !! \033[m")
-        print(self.LexiconNetwork.get_vertex_data())
-        print(self.LexiconNetwork.get_edge_data())
-        
+        self.SemanticNetwork.add_edge_data(edgeDf, vertex_col_names=("src", "dst"))
+        print("[\033[2m+\033[m]\033[1;32m Sucsess to generate semantic network !! \033[m")
+        print(self.SemanticNetwork.get_vertex_data())
+        print(self.SemanticNetwork.get_edge_data())
+
+
+
+    def makePhonologicalNetwork(self):
+        print("[\033[2m+\033[m] Add word in phonological network...")
+        vertDf = cudf.DataFrame({"id":[i for i in range(self.dimention)], "label":self.wordlist, "reservior":cp.zeros(self.dimention), 
+                                 "inflow":cp.zeros(self.dimention), "outflow":cp.zeros(self.dimention)})
+        self.PhonologicalNetwork.add_vertex_data(vertDf, vertex_col_name="id")
+
+        # make edge dataframe
+        edgeDf = cudf.DataFrame({})
+        wordIdPerm = list(itertools.permutations(range(self.dimention), 2))
+        print(len([Id[0] for Id in wordIdPerm]))
+        print(len([Id[1] for Id in wordIdPerm]))
+        edgeDf["src"] = [Id[0] for Id in wordIdPerm]
+        edgeDf["dst"] = [Id[1] for Id in wordIdPerm]
+        edgeDf["distance"] = [Levenshtein.distance(pyopenjtalk.g2p(self.wordlist[Id[0]]), pyopenjtalk.g2p(self.wordlist[Id[1]])) for Id in wordIdPerm]
+        edgeDf = edgeDf[edgeDf.distance < 2]
+        self.PhonologicalNetwork.add_edge_data(edgeDf, vertex_col_names=("src", "dst"))
+        print(edgeDf)
+        #print(self.PhonologicalNetwork.get_vertex_data())
+        #print(self.PhonologicalNetwork.get_edge_data())
 
     def saveLexiconNetwork(self, savePath):
         """Export graph object (pickle)"""
         with open(savePath, "wb") as f:
-            pickle.dump(self.LexiconNetwork, f)
+            pickle.dump(self.SemanticNetwork, f)
             os.chmod(savePath, 755)
             print(f"[\033[2m+\033[m] Save to {savePath}")
         
@@ -71,6 +98,6 @@ if __name__ == "__main__":
         setting = json.load(f)
 
     sn = LexiconNetwork(setting["vector_path"])
-    #sn.makeLexiconNetwork()
+    sn.makePhonologicalNetwork()
     #sn.saveLexiconNetwork(setting["save_path"])
 
